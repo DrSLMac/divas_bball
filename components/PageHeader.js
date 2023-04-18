47083b5dd214be48993f887263a7625eff5c2c09
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2023-04-17 22:06:43 -0500

    set up header for pages

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Inspiration } from 'next/font/google'
-import Image from 'next/image'
-import sheBaller from '../public/she-baller.png'
+import Link from 'next/link'
+import { HomeIcon } from "@heroicons/react/24/solid"
 
 const inspiration = Inspiration({ 
   subsets: ['latin'],
@@ -11,25 +11,29 @@ const inspiration = Inspiration({
 
 const PageHeader = ({pageTitle}) => {
   return (
-    <div>
-        <section className='sticky flex justify-evenly items-center z-20 top-0 h-28 text-white w-screen
-        bg-gradient-to-b from-navy-blue from-20% via-deep-red via-40% to-slate-400
+    <div className='bg-gradient-to-b from-navy-blue from-20% via-deep-red via-40% to-slate-400'>
+        <section className='sticky flex justify-start top-0 px-16 h-28 text-white w-screen
         '>
             <div className={inspiration.className}>
-                <h1 className='text-7xl mt-8' >{pageTitle}</h1>
+                <h1 className='text-7xl mt-6' >{pageTitle}</h1>
             </div>
-            <div>
-                <Image 
-                    src={sheBaller}
-                    alt='Divas Logo'
-                    width='150'
-                    className='mt-48'
-                />
-            </div>
-
         </section>
-
-
+        <div className='flex w-[100%] px-12 py-4
+            '>
+            <button
+                id="basic-button"
+                className='text-white bg-deep-red transition duration-300 ease-in-out rounded-[2rem]
+                hover:bg-navy-blue hover:text-white hover:-translate-y-1 hover:scale-105 hover:shadow-lg
+                h-[2rem] px-5 text-sm
+                '>
+                <Link href='/'>
+                <div className='flex items-center'>
+                    <HomeIcon className="text-white pr-2 h-4 md:h-7 md:w-7"/>
+                    Return to Home
+                </div>
+                </Link>
+            </button>
+        </div>
     </div>
   )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,24 @@
 import PageHeader from '@/components/PageHeader'
 import React from 'react'
 import Footer from '@/components/Footer'
+import sheBaller from '../public/she-baller.png'
+import Image from 'next/image'
+
 
 const About = () => {
   return (
     <div className='relative flex flex-col h-screen'>
         <PageHeader pageTitle='About' />
-
         <section className='h-full bg-slate-400'>
+
+        <div className='absolute right-8 -top-40'>
+          <Image 
+              src={sheBaller}
+              alt='Divas Logo'
+              width='160'
+              className='mt-48'
+          />
+        </div>
 
         </section>
 
