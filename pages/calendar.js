7b403221b49ed0e9d84b082a16e45b4a3a7ef5bf
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2023-04-18 00:34:04 -0500

    added shadows behind baller images

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,25 +1,12 @@
 import PageHeader from '@/components/PageHeader'
 import React from 'react'
 import Footer from '@/components/Footer'
-import sheBaller from '../public/she-baller2.png'
-import hoopBg from '../public/bball-hoop-bg.jpeg'
-import Image from 'next/image'
-
 
 const Calendar = () => {
   return (
     <div className='relative flex flex-col h-screen'>
         <PageHeader pageTitle='Calendar' />
         <section className='h-full bg-bball-hoop bg-center bg-no-repeat bg-cover'>
-
-        {/* <div className='absolute right-10 -top-40'>
-          <Image 
-              src={sheBaller}
-              alt='Divas Logo'
-              width='190'
-              className='mt-48'
-          />
-        </div> */}
 
         </section>
 
