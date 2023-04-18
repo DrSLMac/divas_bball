bb9bbbed1e81199be56ca6f66e45d63cce35f082
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2023-04-17 22:29:24 -0500

    set up about and coaches pages

--- pages/coaches.js
+++ pages/coaches.js
@@ -1,9 +1,30 @@
+import PageHeader from '@/components/PageHeader'
 import React from 'react'
+import Footer from '@/components/Footer'
+import sheBaller from '../public/she-baller2.png'
+import Image from 'next/image'
+
 
 const Coaches = () => {
   return (
-    <div className='p-20'>
-        This is where the Coaches can be found
+    <div className='relative flex flex-col h-screen'>
+        <PageHeader pageTitle='Coaches' />
+        <section className='h-full bg-slate-400'>
+
+        <div className='absolute right-10 -top-40'>
+          <Image 
+              src={sheBaller}
+              alt='Divas Logo'
+              width='190'
+              className='mt-48'
+          />
+        </div>
+
+        </section>
+
+        <section className='sticky bottom-0'>
+            <Footer />
+        </section>
     </div>
   )
 }
