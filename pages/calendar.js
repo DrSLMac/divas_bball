092caa0b0dacab3d7fe1e4715affd198498c44e3
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2023-04-17 22:55:51 -0500

    set up calendar page

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,9 +1,33 @@
+import PageHeader from '@/components/PageHeader'
 import React from 'react'
+import Footer from '@/components/Footer'
+import sheBaller from '../public/she-baller2.png'
+import hoopBg from '../public/bball-hoop-bg.jpeg'
+import Image from 'next/image'
 
-const calendar = () => {
+
+const Calendar = () => {
   return (
-    <div>calendar</div>
+    <div className='relative flex flex-col h-screen'>
+        <PageHeader pageTitle='Calendar' />
+        <section className='h-full bg-bball-hoop bg-center bg-no-repeat bg-cover'>
+
+        {/* <div className='absolute right-10 -top-40'>
+          <Image 
+              src={sheBaller}
+              alt='Divas Logo'
+              width='190'
+              className='mt-48'
+          />
+        </div> */}
+
+        </section>
+
+        <section className='sticky bottom-0'>
+            <Footer />
+        </section>
+    </div>
   )
 }
 
-export default calendar
+export default Calendar
